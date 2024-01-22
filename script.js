1d37c80cae34d4d8603d6c66846cb46bca0e0ac1
PM <121531128+26-PM@users.noreply.github.com>
2024-01-22 13:44:35 +0000

    fixing left library

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-async  function main(){
-    let a=await fetch("https://fantastic-space-trout-9v5prj447r527w69-5500.app.github.dev/songs ")
+async  function getSongs(){
+    let a=await fetch("/songs")
     let response= await a.text();
-    console.log(response)
+    // console.log(response);
+    let div=document.createElement("div");
+    div.innerHTML=response;
+    let as=div.getElementsByTagName("a");
+    let songs=[];
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")){
+            songs.push(element.href);
+        }
+        
+    }
+    return songs;
+}
+
+async function main(){
+    // Get all songs from directory
+    let songs=await getSongs();
+    console.log(songs);
+
+    // Play the song
+    let audio=new Audio(songs[0]);
+    audio.play();
+    audio.pause();
 }
 main();
