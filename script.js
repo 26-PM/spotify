f5b806cc76015327be87d59bbcc7f56d9b867c5c
PM <121531128+26-PM@users.noreply.github.com>
2024-01-23 08:41:17 +0000

    song played

--- script.js
+++ script.js
@@ -1,36 +1,49 @@
-async  function getSongs(){
-    let a=await fetch("/songs")
-    let response= await a.text();
+// Global Variable for curr song
+let currentSong=new Audio();
+
+async function getSongs() {
+    let a = await fetch("/songs")
+    let response = await a.text();
     // console.log(response);
-    let div=document.createElement("div");
-    div.innerHTML=response;
-    let as=div.getElementsByTagName("a");
-    let songs=[];
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let songs = [];
     for (let index = 0; index < as.length; index++) {
         const element = as[index];
-        if (element.href.endsWith(".mp3")){
+        if (element.href.endsWith(".mp3")) {
             songs.push(element.href.split("/songs/")[1]);
         }
-        
+
     }
     return songs;
 }
 
-async function main(){
+const playMusic=(track)=>{
+    // // Play the song
+    currentSong.src="/songs/"+ track + ".mp3";
+    currentSong.play();
+}
+
+async function main() {
     // Get all songs from directory
-    let songs=await getSongs();
+    let songs = await getSongs();
     console.log(songs);
 
     // insert songs
-    let songUL=document.querySelector(".songList").getElementsByTagName("ul")[0];
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
     for (const song of songs) {
-        let cleanSong = song.replaceAll("%20"," ").replace(/%5B128 Kbps%5D-\(SongsPk\.com\.se\)/g, "").replaceAll(".mp3","").replaceAll("(PagalWorld.com.pe)","");
+        let cleanSong = song.replaceAll("%20", " ").replace(/%5B128 Kbps%5D-\(SongsPk\.com\.se\)/g, "").replaceAll(".mp3", "").replaceAll("(PagalWorld.com.pe)", "");
         songUL.innerHTML = songUL.innerHTML + `<li>${cleanSong}</li>`;
-    }    
+    }
 
-    // Play the song
-    let audio=new Audio(songs[0]);
-    audio.play();
-    audio.pause();
+
+    // attach event listener to each song
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach(e=>{
+        e.addEventListener("click",element=>{
+            console.log(e.innerHTML);
+            playMusic(e.innerHTML);
+        })
+    })
 }
 main();
