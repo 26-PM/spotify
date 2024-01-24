20e3e98396b8f766a581b2ecb5cbaf4462c3c1a0
PM <121531128+26-PM@users.noreply.github.com>
2024-01-24 05:49:54 +0000

    check

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let currentSong = new Audio();
 async function getSongs() {
     let a = await fetch("/songs")
     let response = await a.text();
-    // console.log(response);
+    console.log(response);
     let div = document.createElement("div");
     div.innerHTML = response;
     let as = div.getElementsByTagName("a");
